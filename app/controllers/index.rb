041b7381c85ec5ecfce872d994e7623f00646da1
Apprentice <apprentice@dbc-chicago-011.local>
2014-02-26 15:16:32 -0600

    Working on routes and views

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,4 +3,27 @@ get '/' do
   erb :index
 end
 
+get '/sign_in' do
 
+  erb :sign_in
+end
+
+post '/sign_in' do
+
+  user = User.find_by(params[:user][:username])
+
+  if user && user.authenticate(params[:user][:password])
+    session[:user_id] = user.id
+    redirect to ('/')
+  else
+    @errors = {:Invalid => ["Incorrect Login"]}
+    erb: 'sign_in'
+  end
+end
+
+post '/sign_out' do
+
+  session[:user_id] = nil
+
+  redirect to ('/')
+end
